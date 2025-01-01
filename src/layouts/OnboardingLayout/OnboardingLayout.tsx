53c6021f1c2e2967626e04b8e350ab15392348e5
Jihyeon Lee <ljh130334@naver.com>
2025-01-01 23:32:13 +0900

    feat: OnboardingLayout 생성

---
+++ src/layouts/OnboardingLayout/OnboardingLayout.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './OnboardingLayout.module.css';
+import { OnboardingLayoutProps } from './OnboardingLayout.types';
+import KULogo from '../../assets/images/KU-logo.svg';
+
+const OnboardingLayout = ({ children }: OnboardingLayoutProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.logoContainer}>
+          <img 
+            src={KULogo}
+            alt="건국대학교 로고"
+            className={styles.logo}
+          />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default OnboardingLayout;
---
+++ src/layouts/OnboardingLayout/OnboardingLayout.types.ts
@@ -0,0 +1,3 @@
+export interface OnboardingLayoutProps {
+    children: React.ReactNode;
+  }
